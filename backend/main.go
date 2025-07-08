e331ec1c2b5e973459896f629869ba3fe21f5dab
Ardian Permana <ardianpermana075@gmail.com>
2025-07-08 18:00:15 +0700

    feat: init employees repo, service, handler and set up the route

--- backend/main.go
+++ backend/main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"gin/internal/config"
+	"gin/internal/employees"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Load configuration
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("Failed to load config:", err)
 	}
-
-	dbConn, err := config.Connect(cfg.DB)
-	print(dbConn)
+	//connect to DB
+	db, err := config.Connect(cfg.DB)
 	if err != nil {
 		log.Fatal("Failed to connect to DB:", err)
 	}
+
+	// Auto-migrate
+	db.AutoMigrate(&employees.Employee{})
+
+	// Init layer
+	repo := employees.NewRepository(db)
+	service := employees.NewService(repo)
+	handler := employees.NewHandler(service)
+
+	// Set up routes
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	employees.RegisterRoutes(router, handler)
+
+	router.Run(":" + cfg.Port)
 }
